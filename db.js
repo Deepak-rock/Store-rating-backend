a4b72d2ffcb3906016c7210f8853cc05188ca92a
Deepak <deepakind777@gmail.com>
2025-04-13 21:15:30 +0530

    postgres fix test

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-const {Client} = require('pg');
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const db = new Client({
-  host: '127.0.0.1',
-  user: 'postgres',
-  port: 5432,
-  password: 'root123',
-  database: 'ratingPlatfromDB',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect()
-.then(() => console.log('DB Connected'))
-.catch((err) => console.log(err))
-
-module.exports = db;
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.error('DB Connection Error:', err.stack));
