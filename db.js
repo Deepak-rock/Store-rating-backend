5ea64aac6d439b3af5b3c60b24288b7446ef7248
Deepak <deepakind777@gmail.com>
2025-04-13 22:07:33 +0530

    fix db env

--- db.js
+++ db.js
@@ -4,11 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 db.connect()
